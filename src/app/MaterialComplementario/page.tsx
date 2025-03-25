90b4cfd4fb296c4a6c636218fb25e0800da36256
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-03-25 09:35:47 -0600

    trazaurus y material complementario

---
+++ src/app/MaterialComplementario/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <h1 className="bg-main h-screen w-screen flex items-center justify-center text-white text-6xl font-bold">
+      Material complementario
+    </h1>
+  );
+};
+
+export default page;
